bdbcefeb2d8cf47555e36cff220edb9d87a2e88c
Viktoria <zaharova181114@gmail.com>
2022-03-02 19:16:24 +0200

    add pages

--- app/js/common.js
+++ app/js/common.js
@@ -86,3 +86,18 @@ $('.recommended-article-slider').slick({
 
     ]
 });
+
+$('.down').on("click", function () {
+    let $input = $(this).parent().find('input');
+    let count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+});
+$('.up').on("click",function () {
+    let $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+});
