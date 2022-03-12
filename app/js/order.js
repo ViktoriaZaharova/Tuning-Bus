f623fb0366f23fd73184a1b2e746554cb18a0fef
Viktoria <zaharova181114@gmail.com>
2022-03-12 17:31:12 +0200

    edit3

--- app/js/order.js
+++ app/js/order.js
@@ -1,17 +1,59 @@
 $(function () {
-    var topPos = $('.form-total').offset().top;
+    var topPos = $('.form-order').offset().top;
     $(window).scroll(function () {
-        var top = $(document).scrollTop(),
-            pip = $('.advantages-order').offset().top, //расстояние до подвала от верха окна браузера
-            height = $('.form-total').outerHeight(); //получаем значение высоты пл.блока
-        if (top > topPos && top < pip - height) {
-            $('.form-total').addClass('fixed').fadeIn();
-        } //блок будет виден, если значения соответствуют указанным
-        else if (top < pip) {
-            $('.form-total').removeClass('fixed');
-        } //блок скроется когда достигнет заданного расстояния
+        var top = $(document).scrollTop();
+            // pip = $('footer, .advantages-order, .contact-section').offset().top,
+            // height = $('.form-total').outerHeight();
+        if (top > topPos && top) {
+            $('.form-total').addClass('scroll');
+        }
+        // else if (top > pip - height) {
+        //     $('.form-total').removeClass('scroll');
+        // }
+
         else {
-            $('.form-total').removeClass('fixed');
+            $('.form-total').removeClass('scroll');
         }
     });
 });
+
+$(document).ready(function () {
+
+// Получаем нужный элемент
+    var element = document.querySelector('.scroll-stop');
+
+    var visible = function (target) {
+        // Все позиции элемента
+        var targetPosition = {
+                top: window.pageYOffset + target.getBoundingClientRect().top,
+                left: window.pageXOffset + target.getBoundingClientRect().left,
+                right: window.pageXOffset + target.getBoundingClientRect().right,
+                bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+            },
+            // Получаем позиции окна
+            windowPosition = {
+                top: window.pageYOffset,
+                left: window.pageXOffset,
+                right: window.pageXOffset + document.documentElement.clientWidth,
+                bottom: window.pageYOffset + document.documentElement.clientHeight
+            };
+
+        if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+            targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+            targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+            targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+            // Если элемент полностью видно, то запускаем следующий код
+            // console.clear();
+            $('.form-total').removeClass('scroll');
+            console.log('Вы видите элемент :)');
+        }
+    };
+
+// Запускаем функцию при прокрутке страницы
+    window.addEventListener('scroll', function() {
+        visible (element);
+    });
+
+// А также запустим функцию сразу. А то вдруг, элемент изначально видно
+    visible (element);
+})
